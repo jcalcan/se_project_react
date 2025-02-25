6e0d57198ca82304445c2d465daea80af0337129
JUAN ALCANTARA <2481328@gmail.com>
2025-02-24 20:54:18 -0500

    .env variable coded in API

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -2,7 +2,8 @@ import { coordinates } from "./constants";
 
 export class weatherAPI {
   constructor() {
-    this._APIkey = import.meta.env.VITE_TOKEN;
+    // this._APIkey = import.meta.env.VITE_TOKEN;
+    this._APIkey = "100c869ad85fcdbeb33a5d6ec14de055";
     this._baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longtitude}&units=imperial&appid=${this._APIkey}`;
     this._headers = {
       authorization: this._APIkey,
