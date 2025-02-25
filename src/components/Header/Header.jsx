8642a3955eceba65a6b90734101a282b0534d5d3
JUAN ALCANTARA <2481328@gmail.com>
2025-02-24 20:44:31 -0500

    misspelled logo to Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/avatar.svg";
 import { useState } from "react";
 
