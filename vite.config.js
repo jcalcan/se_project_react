7e28dfae7c79c4fb1bdf69790c78c1275930a284
JUAN ALCANTARA <2481328@gmail.com>
2025-02-24 19:26:28 -0500

    change Vite and webpack config to deploy site

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-// import { defineConfig, loadEnv } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 3000 },
-//   define: {
-//     "import.meta.env.VITE_TOKEN": JSON.stringify(env.VITE_TOKEN)
-//   }
-// });
-
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    base: "/se_project_react/",
     plugins: [react()],
     server: { port: 3000 },
     define: {
