b98318a344bf4ec8ed26a3615b4e43fa055371ea
JUAN ALCANTARA <2481328@gmail.com>
2025-02-25 19:32:03 -0500

    revert ".env variable coded in API"
    This reverts commit 6e0d57198ca82304445c2d465daea80af0337129.

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -2,8 +2,7 @@ import { coordinates } from "./constants";
 
 export class WeatherAPI {
   constructor() {
-    // this._APIkey = import.meta.env.VITE_TOKEN;
-    this._APIkey = "100c869ad85fcdbeb33a5d6ec14de055";
+    this._APIkey = import.meta.env.VITE_TOKEN;
     this._baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longtitude}&units=imperial&appid=${this._APIkey}`;
     this._headers = {
       authorization: this._APIkey,
