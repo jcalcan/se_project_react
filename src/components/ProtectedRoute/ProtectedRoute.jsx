b33a8b75d987e5d82922374ba6e32f53f505e9f4
JUAN ALCANTARA <2481328@gmail.com>
2025-05-07 15:56:18 -0400

    import ProtectedRoute.css file into its component

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import AppContext from "../../contexts/AppContext";
+import "./ProtectedRoute.css";
 
 function ProtectedRoute({ children, anonymous = false }) {
   const location = useLocation();
